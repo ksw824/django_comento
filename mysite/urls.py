71879d9e8eb29c326e05f57d01ee43876720a75b
swKim <tjsdnrla1234@gmail.com>
2024-07-26 16:18:01 +0900

    4주차 개발-v2

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('<int:content_id>/', views.detail, name='detail'),
+    path('comment/create/<int:content_id>/', views.comment_create, name='comment_create'),
+
 ]
